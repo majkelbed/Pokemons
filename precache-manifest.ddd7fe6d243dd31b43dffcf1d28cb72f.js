aa9de167b4a0c3e37aa32a58e3b244f16f720e86
Michal Bednarczyk <bednarczyk,business@gmail.com>
2019-06-25 15:34:33 +0200

    Updates

---
+++ precache-manifest.ddd7fe6d243dd31b43dffcf1d28cb72f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "995fd01b063eb71d04f1a7ced4b855e1",
+    "url": "/Pokemons/index.html"
+  },
+  {
+    "revision": "9c6466ec8e35923293a1",
+    "url": "/Pokemons/static/js/2.ad3871d1.chunk.js"
+  },
+  {
+    "revision": "bc8e080f7cc7a2582316",
+    "url": "/Pokemons/static/js/main.2d9ef6d5.chunk.js"
+  },
+  {
+    "revision": "b221a799494df325c9f5",
+    "url": "/Pokemons/static/js/runtime~main.34439cff.js"
+  }
+]);
